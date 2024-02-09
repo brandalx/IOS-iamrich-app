f2ab8db46eb8cd3befd6684f1c26e58e3e0cf867
brndalx <113251192+brandalx@users.noreply.github.com>
2024-02-09 17:24:54 -0500

    feat: first commit

---
+++ i-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-am-rich
+//
+//  Created by Brandon Nolan on 2024-02-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
